770daef66c384ec2d263ab7eb4c25301c2edae8e
hereisSwapnil <swapnilskumars@gmail.com>
2024-06-22 21:42:48 +0530

    removed content.js

--- content.js
+++
@@ -1,9 +0,0 @@
-// const addNoShorts = () => {
-//     console.log("Shorts Guard content script loaded");
-//     let noShortsHeading = document.createElement("h1");
-//     noShortsHeading.classList.add("noShorts")
-//     noShortsHeading.textContent = "No Shorts 😂";
-//     document.body.appendChild(noShortsHeading);
-// }
-
-// addNoShorts();
